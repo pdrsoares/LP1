c4d132f890fb34e85f7e2cc9ecdb04c79051bdb3
pedro soares <pedrodpsoares@gmail.com>
2025-09-09 13:51:31 -0300

    Add files via upload

---
+++ ex2/Cidade.java
@@ -0,0 +1,16 @@
+
+public class Cidade {
+    
+    private String localizacao;
+    private Int populacao;
+    private String nome;
+    
+    
+    
+    public void viajar(String nome) {
+        
+        System.out.println("Estou indo para " + nome + "nesse país lugar melhor não há."); 
+       
+    }
+     
+}
---
+++ ex2/Trabalho.java
@@ -0,0 +1,16 @@
+
+public class Trabalho {
+    
+    private Int salario;
+    private String profissao;
+    private String benficio;
+    
+    
+    
+    public void trabalhar(String profissao) {
+        
+        System.out.println("João do Santo Cristo trabalhava como" + nome); 
+       
+    }
+     
+}
